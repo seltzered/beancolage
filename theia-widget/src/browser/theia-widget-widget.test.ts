7c8c5f6db6a19dffae9b9d0fcb6fe5052e882d39
Vivek Gani <vivek@gani.org>
2022-07-15 16:09:40 -0500

    test: initial update to be working

--- theia-widget/src/browser/theia-widget-widget.test.ts
+++ theia-widget/src/browser/theia-widget-widget.test.ts
@@ -1,8 +1,17 @@
 import 'reflect-metadata';
-import { MessageService } from '@theia/core';
+
+import { FrontendApplicationConfigProvider } from '@theia/core/lib/browser/frontend-application-config-provider';
+import { ApplicationProps } from '@theia/application-package/lib/application-props';
+FrontendApplicationConfigProvider.set({
+    ...ApplicationProps.DEFAULT.frontend.config
+});
+
+import URI from '@theia/core/lib/common/uri';
+import { MiniBrowserOpenHandler, MiniBrowserOpenerOptions } from '@theia/mini-browser/lib/browser/mini-browser-open-handler';
 import { ContainerModule, Container } from '@theia/core/shared/inversify';
 import { TheiaWidgetWidget } from './theia-widget-widget';
 import { render } from '@testing-library/react'
+
 
 describe('TheiaWidgetWidget', () => {
 
@@ -10,11 +19,10 @@ describe('TheiaWidgetWidget', () => {
 
     beforeEach(async () => {
         const module = new ContainerModule( bind => {
-            bind(MessageService).toConstantValue({
-                info(message: string): void {
-                    console.log(message);
+            bind(MiniBrowserOpenHandler).toConstantValue({
+                open(uri: URI, options?: MiniBrowserOpenerOptions): void {
                 }
-            } as MessageService);
+            } as MiniBrowserOpenHandler);
             bind(TheiaWidgetWidget).toSelf();
         });
         const container = new Container();
@@ -24,7 +32,7 @@ describe('TheiaWidgetWidget', () => {
 
     it('should render react node correctly', async () => {
         const element = render(widget.render());
-        expect(element.queryByText('Open Fava Home Tab')).toBeTruthy();
+        expect(element.queryByText('Open Fava Tab')).toBeTruthy();
     });
 
     it('should inject \'MiniBrowserOpenHandler\'', () => {
